0d58ef2755fa812cfebc7cefc679fe19fded85ee
PedroJosue123 <pedro.ponce@tecsup.edu.pe>
2025-06-22 23:27:46 -0500

    Realizar commit3

--- TransActiva/Controllers/BuyerController.cs
+++ TransActiva/Controllers/BuyerController.cs
@@ -13,7 +13,22 @@ public class BuyerController (IOrder order, IPaymentOrder paymentOrder) : Contro
 {
    
   
-  
+    [Authorize(Roles = "Comprador")]
+    [HttpGet("VersOrdenAceptada{id}")]
+    public async Task<IActionResult> VersiPago(int id)
+    {
+        try
+        {
+            var registro = await order.VerSiOrderAceptado(id);
+            return Ok (new { Idpedido = registro });
+            
+        }
+        
+        catch (Exception ex)
+        {
+            return NotFound(new { message = ex.Message });
+        }
+    }
     
     [Authorize(Roles = "Comprador")]
     [HttpPost("VistaPagar")]
