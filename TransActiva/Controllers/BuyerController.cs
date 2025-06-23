b3aeaa89c1704ad042f184da69b37ea7e81b8d4b
PedroJosue123 <pedro.ponce@tecsup.edu.pe>
2025-06-22 23:54:39 -0500

    Ahora de los si

--- TransActiva/Controllers/BuyerController.cs
+++ TransActiva/Controllers/BuyerController.cs
@@ -12,22 +12,7 @@ namespace TransActiva.Controllers;
 public class BuyerController (IOrder order, IPaymentOrder paymentOrder) : ControllerBase
 {
     
-    [Authorize(Roles = "Comprador")]
-    [HttpPost("Pagar")]
-    public async Task<IActionResult> Payment(int id, [FromBody] PaymentCartDto paymentCartDto)
-    {
-        try
-        {
-            var registro = await paymentOrder.Payment(id, paymentCartDto);
-            return Ok (new {registro });
-            
-        }
-        
-        catch (Exception ex)
-        {
-            return NotFound(new { message = ex.Message });
-        }
-    }
+  
     
     [Authorize(Roles = "Comprador")]
     [HttpGet("Mostarlospedidos{id}")]
